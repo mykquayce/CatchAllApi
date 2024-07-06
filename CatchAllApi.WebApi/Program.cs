4dc9e7903b4ca48c4842db07feaef90d7f140a34
bob h <mail@bob.codes>
2024-07-06 19:12:21 +0100

    catch-all endpoint logs entire request

--- CatchAllApi.WebApi/Program.cs
+++ CatchAllApi.WebApi/Program.cs
@@ -1,7 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Map("/{**catchAll}", async (ILogger<Program> logger, HttpRequest request, string? catchAll) => 
+{
+	logger.LogInformation("{time:O}", DateTime.UtcNow);
+	logger.LogInformation("{method} {path}", request.Method, request.Path);
+	foreach (var (key, values) in request.Headers) { logger.LogInformation("Header: {key} = {values}", key, values); }
+	if (request.ContentType != null)
+	{
+		foreach (var (key, values) in request.Form) { logger.LogInformation("Form: {key} = {values}", key, values); }
+	}
+	using var reader = new StreamReader(request.Body);
+	var body = await reader.ReadToEndAsync();
+	logger.LogInformation("Body: {body}", body);
+	return "Hello World!";
+});
 
 app.Run();
 
